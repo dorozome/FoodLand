4af32b3c7e900614617244341546889ca4b6a3fa
dorozome <spiderpig056@gmail.com>
2022-10-06 02:48:22 +0900

    Initial Commit

---
+++ FoodLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodLand
+//
+//  Created by SPIDER PIG on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
